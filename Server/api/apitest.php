2d9c19666621fb452a6d9d153eb4d1a958ee0dd5
Romaniv Petro <romanivpepe@gmail.com>
2015-05-03 21:33:03 +0300

    upgraded API interface

--- Server/api/apitest.php
+++ Server/api/apitest.php
@@ -1,15 +1,16 @@
 <?php
 
 class apitest extends apiBaseClass {
+	
 
-    //http://www.example.com/api/?apitest.helloAPI={}
+    //http://gchat.esy.es/api/?apitest.helloAPI={}
     function helloAPI() {
         $retJSON = $this->createDefaultJson();
         $retJSON->withoutParams = 'It\'s method called without parameters';
         return $retJSON;
     }
 
-    //http://www.example.com/api/?apitest.helloAPIWithParams={"TestParamOne":"Text of first parameter"}
+    //http://gchat.esy.es/api/?apitest.helloAPIWithParams={"TestParamOne":"Text of first parameter"}
     function helloAPIWithParams($apiMethodParams) {
         $retJSON = $this->createDefaultJson();
         if (isset($apiMethodParams->TestParamOne)){
@@ -21,7 +22,7 @@ class apitest extends apiBaseClass {
         return $retJSON;
     }
     
-    //http://www.example.com/api/?apitest.helloAPIResponseBinary={"responseBinary":1}
+    //http://gchat.esy.es/api/?apitest.helloAPIResponseBinary={"responseBinary":1}
     function helloAPIResponseBinary($apiMethodParams){
         header('Content-type: image/png');
         echo file_get_contents("http://habrahabr.ru/i/error-404-monster.jpg");
